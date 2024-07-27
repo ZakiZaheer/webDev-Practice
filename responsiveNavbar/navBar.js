252b0f560a3516751f66d7b4b6f6d3f44ec7904b
ZakiZheer <zaki2232004@gmail.com>
2024-07-27 22:30:20 +0530

    complete navBar

--- responsiveNavbar/navBar.js
+++ responsiveNavbar/navBar.js
@@ -1,9 +1,16 @@
-var headerHeight = document.getElementById('header').clientHeight
-const navLink = document.getElementsByClassName('nav-link')
-for (let i  of navLink) {
-    i.parentElement.style.height = headerHeight + 'px'
+// var headerHeight = document.getElementById('header').clientHeight
+// const navLink = document.getElementsByClassName('nav-link')
+// for (let i  of navLink) {
+//     i.parentElement.style.height = headerHeight + 'px'
 
-    console.log(i.parentElement)
-    console.log(i.parentElement.style.height)
-    console.log(headerHeight)
-}
+//     console.log(i.parentElement)
+//     console.log(i.parentElement.style.height)
+//     console.log(headerHeight)
+// }
+
+
+const navToggle = document.getElementsByClassName('nav-toggle')[0]
+navToggle.addEventListener('click',()=>{
+    const navList = document.getElementsByClassName('nav-list')[0]
+    navList.style.display = 'flex'
+})
